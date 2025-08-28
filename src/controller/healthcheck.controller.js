56da0e07b3fbb6dc6d7440ed5eb52feb55c52f74
gouravs <gourav.shintre@chitaledairy.com>
2025-08-28 17:53:17 +0530

    added asyncHandle Wrapper

--- src/controller/healthcheck.controller.js
+++ src/controller/healthcheck.controller.js
@@ -1,11 +1,18 @@
 import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const healtCheck = (req, res) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse(200, { messgae: "Server is running" }));
-  } catch (error) {}
-};
+// const healtCheck = (req, res) => {
+//   try {
+//     res
+//       .status(200)
+//       .json(new ApiResponse(200, { messgae: "Server is running" }));
+//   } catch (error) {}
+// };
+
+const healtCheck = asyncHandler(async (req, res) => {
+  res
+  .status(200)
+  .json(new ApiResponse (200, {message : "server is running"}))
+});
 
 export { healtCheck };
---
+++ src/models/user.model.js
@@ -0,0 +1,70 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://placehold.co/200`,
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true, //it removes space
+      index: true, //for searching
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); //modified will check which field is modified
+  this.password = await bcrypt.hash(this.password, 10); //10 means it goes upto 10 salt round(hashing algorithm)
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
